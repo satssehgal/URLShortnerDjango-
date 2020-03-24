52a2095d69613c23ae325249c845efd0fc0c0671
satssehgal <satssehgal@gmail.com>
2020-03-23 21:58:52 -0400

    Add files via upload

--- URLShortnerProject/URLShortnerProject/urls.py
+++ URLShortnerProject/URLShortnerProject/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from URLShortnerApp import views
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
